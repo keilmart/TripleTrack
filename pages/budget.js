15cd24c45c3ae80d0fd8754e0c31ba4045f0b65c
keil <keilwmartin@gmail.com>
2025-01-11 11:12:02 -0500

    Clean more out

--- pages/budget.js
+++ pages/budget.js
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout/Layout";
 import BudgetPage from "../components/BudgetPage/BudgetPage";
+import SEO from "../components/SEO/SEO";
 
 const Budget = () => {
   return (
     <Layout wide={true}>
+      <SEO />
       <BudgetPage />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,68 +4,11 @@ import SEO from "../components/SEO/SEO";
 
 const Home = () => {
   return (
-    <>
-      <Layout>
-        <SEO />
-        <Homepage
-        // featuredProjects={featuredProjects}
-        // notableProjects={notableProjects}
-        // comingProjects={comingProjects}
-        // mobileImages={mobileImages}
-        />
-      </Layout>
-    </>
+    <Layout>
+      <SEO />
+      <Homepage />
+    </Layout>
   );
 };
-
-// export async function getStaticProps() {
-//   let notableProjects = [];
-//   let featuredProjects = [];
-//   let comingProjects = [];
-//   let mobileImages = [];
-//   const allProjectData = await getProjectData();
-//   const mobileImagesData = await getProjectData();
-//   console.log("allProjectData", allProjectData.allProjectData);
-
-//   try {
-//     if (allProjectData.allProjectData.length > 0) {
-//       allProjectData.allProjectData.forEach((project) => {
-//         if (project?.comingSoon === true) {
-//           comingProjects.push(project);
-//         } else if (project?.personal === false) {
-//           featuredProjects.push(project);
-//         } else if (project?.personal === true) {
-//           notableProjects.push(project);
-//         } else {
-//           console.log("Project not found", project);
-//         }
-//       });
-//     }
-
-//     if (mobileImagesData.mobileImagesData.length > 0) {
-//       mobileImagesData.mobileImagesData.forEach((project) => {
-//         mobileImages.push(project);
-//       });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     return (props = {
-//       comingProjects: [],
-//       featuredProjects: [],
-//       notableProjects: [],
-//       mobileImages: [],
-//     });
-//   }
-
-//   return {
-//     props: {
-//       comingProjects,
-//       featuredProjects,
-//       notableProjects,
-//       mobileImages,
-//       revalidate: 1,
-//     },
-//   };
-// }
 
 export default Home;
